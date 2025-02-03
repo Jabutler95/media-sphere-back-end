f4b652e0dbd0771aa16b93457017299f7ca7f2e5
Joshua Butler <105134183+Jabutler95@users.noreply.github.com>
2025-02-03 14:51:33 -0500

    create shelf schema

---
+++ models/shelf.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const shelfSchema = new Schema(
+  {
+    books: { type: Schema.Types.ObjectId, ref: 'Book'},
+    shows: { type: Schema.Types.ObjectId, ref: 'Show'},
+    games: { type: Schema.Types.ObjectId, ref: 'Game'},
+    movies: { type: Schema.Types.ObjectId, ref: 'Movie'},
+    owner: { type: Schema.Types.ObjectId, ref: 'Profile'},
+  },
+  { timestamps: true }
+)
+
+const Shelf = mongoose.model('Shelf', shelfSchema)
+
+export { Shelf }
