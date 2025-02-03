5e167c829d31a4e70df9eb9b1f3b7efef3ed1840
Joshua Butler <105134183+Jabutler95@users.noreply.github.com>
2025-02-03 14:36:57 -0500

    build book schema

---
+++ models/book.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: { type: Schema.Types.ObjectId, ref: 'Profile' }
+  },
+  { timestamps: true }
+)
+
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String
+    },
+    genre: {
+      type: String
+    },
+    rating: {
+      enum: ['1','2','3','4','5']
+    },
+    author: {
+      type: String
+    },
+    comments: [commentSchema]
+  },
+  { timestamps: true }
+)
+
+const Book = mongoose.model('Book', bookSchema)
+
+export { Book }
